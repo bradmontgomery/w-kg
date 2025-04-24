d80e7cf025c3b2708d0d8d836f576ed3e0be9434
Brad Montgomery <brad@bradmontgomery.net>
2025-04-23 22:10:35 -0500

    initial feeding algorithm

--- src/FuelingCalculator.jsx
+++ src/FuelingCalculator.jsx
@@ -1,6 +1,9 @@
 import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClock, faBoltLightning, faBolt } from "@fortawesome/free-solid-svg-icons";
+
+const BUCKETSIZE = 30; // feed periods in minutes...
+
 /**
  * brand: Brand name,
  * name: product name,
@@ -15,9 +18,48 @@ const snacks = [
   { brand: "Larabar", name: "Lemon Bar", carbs: 24, calories: 200, serving: 45 },
 ];
 
+// for time (in minutes), caclulate snacks per bucket
+const calculateSnacks = (time, totalCalories) => {
+  const buckets = time / BUCKETSIZE;
+  const bucketCals = Math.round(totalCalories / buckets, 2);
+  let items = [];
+  for (let i = 0; i < buckets; i++) {
+    let _items = [];
+    let _itemCals = 0;
+    while (_itemCals <= bucketCals) {
+      const obj = snacks[Math.floor(Math.random() * snacks.length)];
+      _itemCals += obj.calories;
+      _items.push(obj);
+    }
+
+    items.push({
+      bucket: i,
+      requiredCalories: bucketCals,
+      calories: _items.reduce((accumulator, obj) => accumulator + obj.calories, 0),
+      carbs: _items.reduce((accumulator, obj) => accumulator + obj.carbs, 0),
+      items: _items,
+    });
+  }
+  return items;
+};
+
+function FuelItem({ brand, name, carbs, calories }) {
+  return (
+    <li>
+      {brand} / {name} / {carbs}g carbs / {calories}kcals
+    </li>
+  );
+}
+
+function FuelList(items) {
+  const fuelItems = items.items.map((obj, i) => (
+    <FuelItem key={"item-" + i} brand={obj.brand} name={obj.name} carbs={obj.carbs} calories={obj.calories} />
+  ));
+  return <ul>{fuelItems}</ul>;
+}
+
 function FuelingCalculator() {
-  const [totalCalories, setTotalCalories] = useState(0);
-  const [totalCarbs, setTotalCarbs] = useState(0);
+  const [requiredCalories, setRequiredCalories] = useState(0);
   const [time, setTime] = useState(0);
 
   function handleSubmit(event) {
@@ -29,35 +71,63 @@ function FuelingCalculator() {
     setTime(minutes);
 
     // assume kj == calorie
-    setTotalCalories(parseInt(formData.get("totalwork")));
+    setRequiredCalories(parseInt(formData.get("totalwork")));
   }
 
   function handleReset(event) {
     event.preventDefault();
-    setTotalCalories(0);
-    setTotalCarbs(0);
+    setRequiredCalories(0);
     setTime(0);
   }
 
+  const allSnacks = calculateSnacks(time, requiredCalories);
+  const totalCarbs = allSnacks.reduce((acc, obj) => acc + obj.carbs, 0);
+  const totalCals = allSnacks.reduce((acc, obj) => acc + obj.calories, 0);
+
   const outputTags =
-    totalCalories + totalCarbs > 0 ? (
+    requiredCalories > 0 ? (
       <div className="field is-grouped is-grouped-multiline">
         <div className="control">
-          <div className="tags has-addons is-large">
-            <span className="tag is-warning">{totalCalories}</span>
-            <span className="tag is-dark">calories</span>
+          <div className="tags has-addons">
+            <span className="tag is-large is-warning">{totalCals}</span>
+            <span className="tag is-large is-dark">calories</span>
           </div>
         </div>
         <div className="control">
-          <div className="tags has-addons is-large">
-            <span className="tag is-info">{totalCarbs}</span>
-            <span className="tag is-dark">g</span>
+          <div className="tags has-addons">
+            <span className="tag is-large is-info">{totalCarbs}</span>
+            <span className="tag is-large is-dark">g</span>
           </div>
         </div>
       </div>
     ) : (
       ""
     );
+
+  const food = allSnacks.map((item) => {
+    return (
+      <div>
+        <p className="is-size-4 has-text-weight-light">Feed {item.bucket + 1}</p>
+        <div className="field is-grouped is-grouped-multiline">
+          <div className="control">
+            <div className="tags has-addons">
+              <span className="tag">{item.calories}</span>
+              <span className="tag is-dark">calories</span>
+            </div>
+          </div>
+          <div className="control">
+            <div className="tags has-addons">
+              <span className="tag">{item.carbs}</span>
+              <span className="tag is-dark">g carbs</span>
+            </div>
+          </div>
+        </div>
+
+        <FuelList items={[...item.items]} />
+        <hr />
+      </div>
+    );
+  });
 
   return (
     <>
@@ -68,11 +138,17 @@ function FuelingCalculator() {
       </h1>
       <p className="subtitle">Counting the Calroies &amp; Carbs for you.</p>
       <div className="columns">
-        <div class="column">
+        <div className="column">
           <form action="#" onSubmit={handleSubmit}>
             <div className="field has-addons">
               <div className="control has-icons-left">
-                <input className="input" type="text" name="ridetime" placeholder="Total ride time" />
+                <input
+                  className="input"
+                  type="text"
+                  name="ridetime"
+                  placeholder="Total ride time"
+                  defaultValue={time}
+                />
                 <span className="icon is-small is-left">
                   <FontAwesomeIcon icon={faClock} />
                 </span>
@@ -83,7 +159,13 @@ function FuelingCalculator() {
             </div>
             <div className="field has-addons">
               <div className="control has-icons-left">
-                <input className="input" type="text" name="totalwork" placeholder="Expected total work" />
+                <input
+                  className="input"
+                  type="text"
+                  name="totalwork"
+                  placeholder="Expected total work"
+                  defaultValue={totalCals}
+                />
                 <span className="icon is-small is-left">
                   <FontAwesomeIcon icon={faBoltLightning} />
                 </span>
@@ -109,9 +191,10 @@ function FuelingCalculator() {
             </div>
           </form>
         </div>
-        <div class="column">
-          {time && <h2>For {time} total minutes...</h2>}
+        <div className="column">
+          {time > 0 && <h2>For {time} total minutes...</h2>}
           {outputTags}
+          <div>{food}</div>
         </div>
       </div>
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -1,30 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-import Root from './Root.jsx';
-import ErrorBoundary from './ErrorBoundary.jsx';
-import WattPerKg from './WattsPerKg.jsx';
-import FuelingCalculator from './FuelingCalculator.jsx';
+import Root from "./Root.jsx";
+import ErrorBoundary from "./ErrorBoundary.jsx";
+import WattPerKg from "./WattsPerKg.jsx";
+import FuelingCalculator from "./FuelingCalculator.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/w-kg/',
+    path: "/w-kg/",
     Component: Root,
     ErrorBoundary: ErrorBoundary,
     children: [
       { index: true, Component: WattPerKg },
-      { path: 'fueling', Component: FuelingCalculator }
-    ]
-  }
+      { path: "fueling", Component: FuelingCalculator },
+    ],
+  },
 ]);
 
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={router} />
-  </StrictMode>,
-)
+  </StrictMode>
+);
